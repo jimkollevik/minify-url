ca9e093a4b9ad509f37ea531e850e4ef8e209593
Jim Kollevik <jim.kollevik@gmail.com>
2022-08-30 13:37:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -23,3 +23,12 @@ function geturl() {
 	});
   }
 }
+
+$("button").on("touchsart mousedown", function () {
+	$(this).addClass("clicked");
+});
+
+$("button").on("touchend mouseup", function () {
+	$(this).removeClass("clicked");
+	$(this).blur();
+});
