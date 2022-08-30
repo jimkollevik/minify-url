729a274985bc8bd806684f39992d3997142ad3dc
Jim Kollevik <jim.kollevik@gmail.com>
2022-08-30 13:13:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,25 @@
 function geturl() {
  var url = document.getElementById("urlinput").value;
  var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
- 	if(!protocol_ok) {
-		console.log("Denied")
+   if(!protocol_ok) {
+	   console.log("Please use an URL that starts with either http://, https:// or ftp://")
   	}
-  else {
-  
-		var url = "https://api.shrtco.de/v2/shorten?url=" + url;
-
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", url);
-
-		xhr.setRequestHeader("Accept", "application/json");
-		xhr.setRequestHeader("Content-Type", "application/json");
-
-		xhr.onreadystatechange = function () {
-   			if (xhr.readyState === 4) {
-      			console.log(xhr.status);
-      			console.log(xhr.responseText);
-   		}};
-		console.log("Success")
-	  $.getJSON(url, function(data) {
-		  var uri = data.result.short_link;
-		  console.log(uri)
-	  }
-  })
+   else {
+    var url = "https://api.shrtco.de/v2/shorten?url=" + url;
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.onreadystatechange = function () {
+	  if (xhr.readyState === 4) {
+	  console.log(xhr.status);
+       	  console.log(xhr.responseText);
+   	}
+    };
+    console.log("Success")
+    $.getJSON(url, function(data) {
+	var uri = data.result.short_link;
+	    $( "#shortUrl" ).append(uri);
+	});
+  }
 }
