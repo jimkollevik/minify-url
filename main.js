9e46ad550598367c494a53a2935d8e4b120c9028
Jim Kollevik <jim.kollevik@gmail.com>
2022-08-30 09:26:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,67 +1,23 @@
-var endpoint = "https://api.jsonstores.com/AA881541393665888256c6EY";
-
 function geturl() {
  var url = document.getElementById("urlinput").value;
  var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
  	if(!protocol_ok) {
-  	newurl = "http://"+url;
-    	return newurl;
-  }
+		var url = "https://api.shrtco.de/v2/shorten?url=" + url;
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", url);
+
+		xhr.setRequestHeader("Accept", "application/json");
+		xhr.setRequestHeader("Content-Type", "application/json");
+
+		xhr.onreadystatechange = function () {
+   			if (xhr.readyState === 4) {
+      			console.log(xhr.status);
+      			console.log(xhr.responseText);
+   		}};
+		console.log("Success")
+  	}
   else {
-  return url;
+  console.log("Denied")
   }
-}
-
-function getrandom() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    return text;
-}
-
-function genhash() {
-	if(window.location.hash == "") {
-  	window.location.hash = getrandom();
-  }
-}
-
-function send_request(url) {
-
-//new code
-	
-
-let xhr = new XMLHttpRequest()
-
-let myObj = {url:endpoint + "/" + window.location.hash.substr(1)};                                            
-xhr.addEventListener('load', () => {    
-	if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.responseText)
-    }                                      
-})
-                                            
-let url =   endpoint + "/" + window.location.hash.substr(1)
-xhr.open('POST', url, false)
-                                        
-xhr.send(JSON.stringify(myObj))
-//end new code
-}
-
-function shorturl() {
-	var longurl = geturl();
-  genhash();
-  send_request(longurl);
-}
-
-var hashh = window.location.hash.substr(1)
-
-if (window.location.hash != "") {
-	$.getJSON(endpoint + "/" + hashh, function (data) {
-  	data = data["result"];
-    
-	if (data != null) {
-	window.location.href = data;
-  }
-});
 }
