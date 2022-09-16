4769ebd13bddec49be94d44f3ba74f97802ccff3
Jim Kollevik <jim.kollevik@gmail.com>
2022-09-16 14:19:30 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -16,11 +16,28 @@ function geturl() {
        	  console.log(xhr.responseText);
    	}
     };
-    console.log("Success")
     $.getJSON(url, function(data) {
 	var uri = data.result.short_link;
 	    $( "#shortUrl" ).append(uri);
+	    $( ".switch" ).css("display", "none");
+	    $( "#newUrl" ).append("There it is, short and nice. Click the link to copy.");
+	    return uri;
 	});
   }
+}
+
+function copyUrl() {
+  // Get the text field
+  var copyText = document.getElementById("shortUrl");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
 }
 
